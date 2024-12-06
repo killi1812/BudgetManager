aadc1a252e6c11307993862554adf648f864e156
killi1812 <cvok.fran@gmail.com>
2024-12-06 10:05:49 +0100

    Log time

--- Data/Models/Log.cs
+++ Data/Models/Log.cs
@@ -7,7 +7,7 @@ public partial class Log
 {
     public int Id { get; set; }
 
-    public DateTime Date { get; set; }
+    public DateTime Date { get; set; } = DateTime.Now;
 
     public string Message { get; set; } = null!;
 

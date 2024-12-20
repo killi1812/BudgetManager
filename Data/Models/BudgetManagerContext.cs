1fde851b3d280ba05a1720951f3649ab88a9dac6
killi1812 <cvok.fran@gmail.com>
2024-12-20 08:18:58 +0100

    Removed conn string

--- Data/Models/BudgetManagerContext.cs
+++ Data/Models/BudgetManagerContext.cs
@@ -35,10 +35,6 @@ public partial class BudgetManagerContext : DbContext
 
     public virtual DbSet<User> Users { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=localhost,1433;Database=BudgetManager;User=sa;Password=password123!;Encrypt=False;TrustServerCertificate=False");
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<BankAccountApi>(entity =>
